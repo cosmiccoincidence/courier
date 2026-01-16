c07cc29f0d029a141635d49f9814940f2ab7f422
CosmicCoincidence <cosmiccoincidence1991@gmail.com>
2026-01-15 21:53:42 -0500

    adjust min zoom

--- Characters/Player/player_camera.gd
+++ Characters/Player/player_camera.gd
@@ -7,7 +7,7 @@ var camera: Camera3D
 
 # ===== ZOOM =====
 @export_group("Zoom")
-@export var zoom_min: int = 50
+@export var zoom_min: int = 25
 @export var zoom_max: int = 75
 @export var zoom_speed: float = 15.0
 @export var zoom_smooth: float = 8.0
